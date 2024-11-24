fdcfdfa1f6895403a9b3ba67aeafe437d03e6f83
Luciano <lucianorodriguess101@gmail.com>
2024-11-24 13:42:13 -0300

    feat(usecase): implement GetSlotMachineBalanceUseCase for retrieving slot machine balance

---
+++ internal/application/usecase/get_slot_machine_balance_usecase.go
@@ -0,0 +1,36 @@
+package usecase
+
+import (
+	"context"
+	"slot-machine/internal/domain/model"
+	"slot-machine/internal/domain/repository"
+)
+
+type GetSlotMachineBalanceUseCase struct {
+	SlotMachineRepo repository.SlotMachineRepository
+}
+
+type GetSlotMachineBalanceRequest struct {
+	MachineID string `json:"machine_id"`
+}
+
+type GetSlotMachineBalanceResponse struct {
+	Machine model.SlotMachine `json:"machine"`
+}
+
+func NewGetSlotMachineBalanceUseCase(smr repository.SlotMachineRepository) *GetSlotMachineBalanceUseCase {
+	return &GetSlotMachineBalanceUseCase{
+		SlotMachineRepo: smr,
+	}
+}
+
+func (uc *GetSlotMachineBalanceUseCase) Execute(ctx context.Context, req *GetSlotMachineBalanceRequest) (*GetSlotMachineBalanceResponse, error) {
+	machine, err := uc.SlotMachineRepo.GetSlotMachine(ctx, req.MachineID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GetSlotMachineBalanceResponse{
+		Machine: *machine,
+	}, nil
+}
