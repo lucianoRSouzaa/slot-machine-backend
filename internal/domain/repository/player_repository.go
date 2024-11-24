34243ad6ae7850175157b982efcddd6d74ac1354
Luciano <lucianorodriguess101@gmail.com>
2024-11-24 12:16:54 -0300

    feat(repository): add PlayerRepository interface with CRUD methods

---
+++ internal/domain/repository/player_repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"slot-machine/internal/domain/model"
+)
+
+var (
+	ErrPlayerNotFound = errors.New("player not found")
+)
+
+type PlayerRepository interface {
+	CreatePlayer(ctx context.Context, player *model.Player) error
+	GetPlayer(ctx context.Context, id string) (*model.Player, error)
+	UpdatePlayer(ctx context.Context, player *model.Player) error
+}
