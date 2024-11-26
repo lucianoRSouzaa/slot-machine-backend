c733dc6828e44cbd24445a73c516569e3b0a1092
Luciano <lucianorodriguess101@gmail.com>
2024-11-26 20:21:11 -0300

    feat(model): add Email and Password fields to Player struct

--- internal/domain/model/player.go
+++ internal/domain/model/player.go
@@ -1,6 +1,8 @@
 package model
 
 type Player struct {
-	ID      string `json:"id"`
-	Balance int    `json:"balance"`
+	ID       string `json:"id"`
+	Balance  int    `json:"balance"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
