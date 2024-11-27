00423f7e52ee859435decc646a4a34a94327a74a
Luciano <lucianorodriguess101@gmail.com>
2024-11-26 21:28:58 -0300

    feat(handler): set HTTP status to 201 Created for CreatePlayer response

--- internal/adapters/http/handler.go
+++ internal/adapters/http/handler.go
@@ -100,6 +100,8 @@ func (h *Handler) CreatePlayer(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(resp)
 }
 
