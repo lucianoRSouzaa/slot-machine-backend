36cd30c49eb4e0a88f8174558b60443b6e8ca0ac
Luciano <lucianorodriguess101@gmail.com>
2024-11-26 21:28:35 -0300

    refactor(model): hide Password field in Player struct from JSON serialization

--- internal/domain/model/player.go
+++ internal/domain/model/player.go
@@ -4,5 +4,5 @@ type Player struct {
 	ID       string `json:"id"`
 	Balance  int    `json:"balance"`
 	Email    string `json:"email"`
-	Password string `json:"password"`
+	Password string `json:"-"`
 }
