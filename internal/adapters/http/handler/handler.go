650d4ad7e30f5406db32342f0aa0861b0020295f
Luciano <lucianorodriguess101@gmail.com>
2024-12-01 16:06:58 -0300

    refactor(handler): update error handling to use centralized HTTPError structure

--- internal/adapters/http/handler/handler.go
+++ internal/adapters/http/handler/handler.go
@@ -3,18 +3,11 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	handler_error "slot-machine/internal/adapters/http/handler/error"
+	"slot-machine/internal/adapters/http/middleware"
 	"slot-machine/internal/application/usecase"
 	"slot-machine/internal/domain/repository"
 )
-
-// HTTPError representa um erro retornado pela API.
-// @Description Estrutura para representar erros na API.
-// @Description Contém a mensagem de erro e um código opcional.
-// @Description Pode ser expandida conforme necessário.
-type HTTPError struct {
-	Code    int    `json:"code"`    // Código do erro HTTP
-	Message string `json:"message"` // Mensagem descritiva do erro
-}
 
 type Handler struct {
 	CreatePlayerUseCase          *usecase.CreatePlayerUseCase
@@ -51,29 +44,40 @@ func NewHandler(
 // @Produce json
 // @Param playRequest body usecase.PlayRequest true "Dados da jogada"
 // @Success 200 {object} usecase.PlayResponse "Jogada realizada com sucesso"
-// @Failure 400 {object} HTTPError "Payload inválido"
-// @Failure 404 {object} HTTPError "Máquina caça-níqueis não encontrada"
-// @Failure 422 {object} HTTPError "Saldo insuficiente"
-// @Failure 500 {object} HTTPError "Erro interno do servidor"
+// @Failure 400 {object} handler_error.HTTPError "Payload inválido"
+// @Failure 404 {object} handler_error.HTTPError "Máquina caça-níqueis não encontrada"
+// @Failure 422 {object} handler_error.HTTPError "Saldo insuficiente"
+// @Failure 500 {object} handler_error.HTTPError "Erro interno do servidor"
 // @Router /play [post]
 // @Security BearerAuth
 func (h *Handler) PlaySlotMachine(w http.ResponseWriter, r *http.Request) {
 	var req usecase.PlayRequest
 	w.Header().Set("Content-Type", "application/json")
 
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(HTTPError{
-			Code:    http.StatusBadRequest,
-			Message: "Invalid request payload",
+	userID, err := middleware.GetUserIDFromContext(r.Context())
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(handler_error.HTTPError{
+			Code:    http.StatusUnauthorized,
+			Message: "Unauthorized",
 		})
-
 		return
 	}
 
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(handler_error.HTTPError{
+			Code:    http.StatusBadRequest,
+			Message: "Invalid request payload",
+		})
+		return
+	}
+
+	req.PlayerID = userID
+
 	response, err := h.PlayUseCase.Execute(r.Context(), &req)
 	if err != nil {
-		handleError(w, err)
+		handler_error.HandleError(w, err)
 		return
 	}
 
@@ -88,9 +92,9 @@ func (h *Handler) PlaySlotMachine(w http.ResponseWriter, r *http.Request) {
 // @Produce json
 // @Param createPlayerRequest body usecase.CreatePlayerRequest true "Dados do jogador a ser criado"
 // @Success 201 {object} usecase.CreatePlayerResponse "Jogador criado com sucesso"
-// @Failure 400 {object} HTTPError "Payload inválido"
-// @Failure 409 {object} HTTPError "Jogador já existe"
-// @Failure 500 {object} HTTPError "Erro interno do servidor"
+// @Failure 400 {object} handler_error.HTTPError "Payload inválido"
+// @Failure 409 {object} handler_error.HTTPError "Jogador já existe"
+// @Failure 500 {object} handler_error.HTTPError "Erro interno do servidor"
 // @Router /players [post]
 func (h *Handler) CreatePlayer(w http.ResponseWriter, r *http.Request) {
 	var req usecase.CreatePlayerRequest
@@ -98,7 +102,7 @@ func (h *Handler) CreatePlayer(w http.ResponseWriter, r *http.Request) {
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(HTTPError{
+		json.NewEncoder(w).Encode(handler_error.HTTPError{
 			Code:    http.StatusBadRequest,
 			Message: "Invalid request payload",
 		})
@@ -110,7 +114,7 @@ func (h *Handler) CreatePlayer(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		if err == usecase.ErrPlayerAlreadyExists {
 			w.WriteHeader(http.StatusConflict)
-			json.NewEncoder(w).Encode(HTTPError{
+			json.NewEncoder(w).Encode(handler_error.HTTPError{
 				Code:    http.StatusConflict,
 				Message: "Player already exists",
 			})
@@ -118,7 +122,7 @@ func (h *Handler) CreatePlayer(w http.ResponseWriter, r *http.Request) {
 			return
 		} else if err == usecase.ErrValidate {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(HTTPError{
+			json.NewEncoder(w).Encode(handler_error.HTTPError{
 				Code:    http.StatusBadRequest,
 				Message: "email and password must be provided",
 			})
@@ -126,7 +130,7 @@ func (h *Handler) CreatePlayer(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(HTTPError{
+		json.NewEncoder(w).Encode(handler_error.HTTPError{
 			Code:    http.StatusInternalServerError,
 			Message: "Unable to create player",
 		})
@@ -146,8 +150,8 @@ func (h *Handler) CreatePlayer(w http.ResponseWriter, r *http.Request) {
 // @Produce json
 // @Param createSlotMachineRequest body usecase.CreateSlotMachineRequest true "Dados da máquina caça-níqueis a ser criada"
 // @Success 201 {object} usecase.CreateSlotMachineResponse "Máquina criada com sucesso"
-// @Failure 400 {object} HTTPError "Payload inválido ou parâmetros inválidos"
-// @Failure 500 {object} HTTPError "Erro interno do servidor"
+// @Failure 400 {object} handler_error.HTTPError "Payload inválido ou parâmetros inválidos"
+// @Failure 500 {object} handler_error.HTTPError "Erro interno do servidor"
 // @Router /machines [post]
 // @Security BearerAuth
 func (h *Handler) CreateSlotMachine(w http.ResponseWriter, r *http.Request) {
@@ -156,7 +160,7 @@ func (h *Handler) CreateSlotMachine(w http.ResponseWriter, r *http.Request) {
 	var req usecase.CreateSlotMachineRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(HTTPError{
+		json.NewEncoder(w).Encode(handler_error.HTTPError{
 			Code:    http.StatusBadRequest,
 			Message: "Invalid request payload",
 		})
@@ -168,7 +172,7 @@ func (h *Handler) CreateSlotMachine(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		if err == usecase.ErrSlotMachineAlreadyExists {
 			w.WriteHeader(http.StatusConflict)
-			json.NewEncoder(w).Encode(HTTPError{
+			json.NewEncoder(w).Encode(handler_error.HTTPError{
 				Code:    http.StatusConflict,
 				Message: "Slot machine already exists",
 			})
@@ -176,7 +180,7 @@ func (h *Handler) CreateSlotMachine(w http.ResponseWriter, r *http.Request) {
 			return
 		} else if err == usecase.ErrValidate {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(HTTPError{
+			json.NewEncoder(w).Encode(handler_error.HTTPError{
 				Code:    http.StatusBadRequest,
 				Message: "level, multiple gain, and description must be provided",
 			})
@@ -185,7 +189,7 @@ func (h *Handler) CreateSlotMachine(w http.ResponseWriter, r *http.Request) {
 		}
 
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(HTTPError{
+		json.NewEncoder(w).Encode(handler_error.HTTPError{
 			Code:    http.StatusInternalServerError,
 			Message: "Unable to create slot machine",
 		})
@@ -205,9 +209,9 @@ func (h *Handler) CreateSlotMachine(w http.ResponseWriter, r *http.Request) {
 // @Produce json
 // @Param loginRequest body usecase.LoginRequest true "Dados de autenticação"
 // @Success 200 {object} usecase.LoginResponse
-// @Failure 400 {object} HTTPError "Requisição inválida"
-// @Failure 401 {object} HTTPError "Credenciais inválidas"
-// @Failure 500 {object} HTTPError "Erro interno do servidor"
+// @Failure 400 {object} handler_error.HTTPError "Requisição inválida"
+// @Failure 401 {object} handler_error.HTTPError "Credenciais inválidas"
+// @Failure 500 {object} handler_error.HTTPError "Erro interno do servidor"
 // @Router /login [post]
 func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	var req usecase.LoginRequest
@@ -280,27 +284,4 @@ func (h *Handler) GetSlotMachineBalance(w http.ResponseWriter, r *http.Request) 
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(resp)
-}
-
-func handleError(w http.ResponseWriter, err error) {
-	switch err {
-	case usecase.ErrInsufficientBalance:
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		json.NewEncoder(w).Encode(HTTPError{
-			Code:    http.StatusUnprocessableEntity,
-			Message: "Insufficient balance",
-		})
-	case repository.ErrPlayerNotFound, repository.ErrSlotMachineNotFound:
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(HTTPError{
-			Code:    http.StatusNotFound,
-			Message: "Resource not found",
-		})
-	default:
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(HTTPError{
-			Code:    http.StatusInternalServerError,
-			Message: "Internal server error",
-		})
-	}
 }
