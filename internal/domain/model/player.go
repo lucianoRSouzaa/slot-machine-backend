34ff31190bc3645cbdf745d0a34e8bdf65115f26
Luciano <lucianorodriguess101@gmail.com>
2024-12-01 16:44:58 -0300

    feat(model): add Role field to Player struct with predefined roles

--- internal/domain/model/player.go
+++ internal/domain/model/player.go
@@ -1,8 +1,16 @@
 package model
 
+type Role string
+
+const (
+	PlayerRole Role = "player"
+	AdminRole  Role = "admin"
+)
+
 type Player struct {
 	ID       string `json:"id"`
 	Balance  int    `json:"balance"`
 	Email    string `json:"email"`
 	Password string `json:"-"`
+	Role     Role   `json:"-"`
 }
