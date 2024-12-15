9673bf77ebdff3f048fcda9af5f0debebe44ff85
Luciano <lucianorodriguess101@gmail.com>
2024-12-15 19:15:11 -0300

    feat(domain): enhance JWTManager with access and refresh token generation and verification methods

--- internal/infrastructure/jwt/jwt_manager.go
+++ internal/infrastructure/jwt/jwt_manager.go
@@ -2,65 +2,103 @@ package jwt
 
 import (
 	"errors"
+	"fmt"
 	"slot-machine/internal/domain/ports"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
-type JWTClaims struct {
-	UserID string `json:"user_id"`
-	jwt.StandardClaims
-}
 
 type JWTManager struct {
 	secretKey     string
-	tokenDuration time.Duration
+	accessTokenDuration  time.Duration
+    refreshTokenDuration time.Duration
 }
 
-func NewJWTManager(secretKey string, tokenDuration time.Duration) ports.JWTManager {
+func NewJWTManager(secretKey string, accessTokenDuration, refreshTokenDuration time.Duration) ports.JWTManager {
 	return &JWTManager{
-		secretKey:     secretKey,
-		tokenDuration: tokenDuration,
-	}
+        secretKey:            secretKey,
+        accessTokenDuration:  accessTokenDuration,
+        refreshTokenDuration: refreshTokenDuration,
+    }
 }
 
-func (m *JWTManager) Generate(userID string) (string, error) {
-	claims := &JWTClaims{
-		UserID: userID,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(m.tokenDuration).Unix(),
-			IssuedAt:  time.Now().Unix(),
-		},
-	}
+func (m *JWTManager) GenerateAccessToken(userID string) (string, error) {
+    claims := &ports.JWTClaims{
+        UserID:    userID,
+        TokenType: ports.TokenTypeAccess,
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: time.Now().Add(m.accessTokenDuration).Unix(),
+            IssuedAt:  time.Now().Unix(),
+        },
+    }
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(m.secretKey))
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString([]byte(m.secretKey))
 }
 
-func (m *JWTManager) Verify(token string) (*ports.JWTClaims, error) {
-	claims := &JWTClaims{}
+func (m *JWTManager) GenerateRefreshToken(userID string) (string, error) {
+    claims := &ports.JWTClaims{
+        UserID:    userID,
+        TokenType: ports.TokenTypeRefresh,
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: time.Now().Add(m.refreshTokenDuration).Unix(),
+            IssuedAt:  time.Now().Unix(),
+        },
+    }
 
-	parsedToken, err := jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (interface{}, error) {
-		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("unexpected signing method")
-		}
-		return []byte(m.secretKey), nil
-	})
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString([]byte(m.secretKey))
+}
 
-	if err != nil {
-		return nil, err
-	}
+func (m *JWTManager) VerifyAccessToken(tokenString string) (*ports.JWTClaims, error) {
+    claims, err := m.verifyToken(tokenString)
+    if err != nil {
+        return nil, err
+    }
+    if claims.TokenType != ports.TokenTypeAccess {
+        return nil, errors.New("invalid token type")
+    }
+    return claims, nil
+}
 
-	if !parsedToken.Valid {
-		return nil, errors.New("invalid token")
-	}
+func (m *JWTManager) VerifyRefreshToken(tokenString string) (*ports.JWTClaims, error) {
+    claims, err := m.verifyToken(tokenString)
+    if err != nil {
+        return nil, err
+    }
+    if claims.TokenType != ports.TokenTypeRefresh {
+        return nil, errors.New("invalid token type")
+    }
+    return claims, nil
+}
+func (m *JWTManager) verifyToken(tokenString string) (*ports.JWTClaims, error) {
+    claims := &ports.JWTClaims{}
 
-	return &ports.JWTClaims{
-		UserID: claims.UserID,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: claims.ExpiresAt,
-			IssuedAt:  claims.IssuedAt,
-		},
-	}, nil
+    parsedToken, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+        if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, errors.New("unexpected signing method")
+        }
+        return []byte(m.secretKey), nil
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+	fmt.Println(parsedToken)
+
+    if !parsedToken.Valid {
+        return nil, errors.New("invalid token")
+    }
+
+    return &ports.JWTClaims{
+        UserID: claims.UserID,
+		TokenType: claims.TokenType,
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: claims.ExpiresAt,
+            IssuedAt:  claims.IssuedAt,
+        },
+    }, nil
 }
